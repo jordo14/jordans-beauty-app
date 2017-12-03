9310e937d7d56a3f5583a16c9a1eb624a8567d07
Jordan Wills <jordanwills@admins-MacBook-Pro.local>
2017-12-02 18:10:28 -0600

    Adds Styling to product details pages, adds photos to every product, adds review plugin to product details pages;WIP

--- wp-content/themes/jordans-beauty-site/templates/content-single.php
+++ wp-content/themes/jordans-beauty-site/templates/content-single.php
@@ -1,26 +1,35 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <header>
+            <h1 class="entry-title my-title"><?php the_title(); ?></h1>
+          </header>
+          <div class="thumbnail">
+            <?php the_post_thumbnail(); ?>
+          </div>
+        </div>
+        <div class="col align-self-center">
+          <h2>Description</h2>
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
+          <div>
+          <p><b>Brand:</b> <?php the_field("brand") ?> </p>
+          </div>
+          <div>
+          <p><b>Price:</b> $<?php the_field("price") ?> </p>
+          </div>
+          <div>
+          <a href="<?php the_field("product_link") ?>"><?php the_field("product_link") ?></a>
+          </div>
+        </div>
+      </div>  
     </div>
+    <h2>Reviews</h2>
     <div>
-      <?php the_post_thumbnail(); ?>
-    </div>
-    <div>
-    <?php the_field("brand") ?>
-    </div>
-    <div>
-    <?php the_field("price") ?>
-    </div>
-    <div>
-    <?php the_field("product_link") ?>
-    </div>
-    <div>
-    <?php the_field("product_type") ?>
+    <?php echo do_shortcode( ' [submit-review] ' ); ?>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
